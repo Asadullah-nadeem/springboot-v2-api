6397db28fefd9e0a2b7b5d6c57c851153ba91214
Muhammad <asadullahnadeem48@gmail.com>
2025-06-08 15:17:59 +0530

    ADD code

---
+++ src/main/java/com/asadullah/TestApiv2/TestApiV2Application.java
@@ -0,0 +1,13 @@
+package com.asadullah.TestApiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/asadullah/TestApiv2/TestApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asadullah.TestApiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
